c5afd0f3d8c6cab00355fdf0e60024462d577f67
Zeyu He <hezzze@gmail.com>
2016-07-27 22:22:57 -0400

    fix: box2dweb lib incompatiblity

--- src/vendor.ts
+++ src/vendor.ts
@@ -12,10 +12,8 @@ import 'rxjs';
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
 
-require('../lib/dump.js');
-require('../lib/clipper.js');
-
 //there is a weird bug for clipper.js if putting box2dweb at the beginning
 // which will make .AddPolygons become undefined
 require('../lib/Box2dWeb-2.1.a.3.js');
+require('../lib/clipper.js');
 require('../lib/poly2tri.js');
